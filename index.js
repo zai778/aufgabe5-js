a47daf41771b67a8f967bcbedd40e6b2deddc7a0
zaid <zaedking445@gmail.com>
2022-07-05 18:13:39 +0200

    random

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const random = Math.random() * 12 + 1;
+const random = Math.floor(Math.random() * 12 + 1);
 if (random <= 6) {
   console.log("Ja!");
 } else {
   console.log("Nein!");
 }
-console.log("banane");
+console.log(random);
